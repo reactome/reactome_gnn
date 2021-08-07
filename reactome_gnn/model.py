af1e360db6d5142ed26badc527e03d90a3a31a0c
Lovro Vrcek <vrcek.lovro@gmail.com>
2021-08-07 12:26:18 +0800

    Refactored loss plotting

--- reactome_gnn/model.py
+++ reactome_gnn/model.py
@@ -10,7 +10,7 @@ class GCNModel(nn.Module):
     and its features, first 
     """
 
-    def __init__(self, dim_latent, num_layers=1):
+    def __init__(self, dim_latent, num_layers=1, train=False):
         """
         Parameters
         ----------
@@ -26,6 +26,7 @@ class GCNModel(nn.Module):
         self.relu = nn.LeakyReLU()
         self.layers = nn.ModuleList([GraphConv(dim_latent, dim_latent, allow_zero_in_degree=True)
                                      for _ in range(num_layers - 1)])
+        self.predict = nn.Linear(dim_latent, 1)
 
     def forward(self, graph):
         """Return the embedding of a graph."""
@@ -39,4 +40,8 @@ class GCNModel(nn.Module):
         for conv in self.layers:
             embedding = self.relu(embedding)
             embedding = conv(graph, embedding)
-        return embedding.detach()
+        if self.train:
+            logits = self.predict(embedding)
+            return logits
+        else:
+            return embedding.detach()
--- reactome_gnn/train.py
+++ reactome_gnn/train.py
@@ -5,11 +5,13 @@ import os
 import pickle
 import random
 import time
+from dgl.batch import _batch_feat_dicts, batch
 
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import random_split
 from dgl.dataloading import GraphDataLoader
@@ -40,14 +42,60 @@ def draw_loss_plot(train_loss, valid_loss, timestamp):
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
-    plt.savefig(f'figures/loss_{timestamp}.png')
+    plt.savefig(f'loss_{timestamp}.png')
     plt.show()
 
 def train():
     path = ''
+    dim_latent = 8
+    num_layers = 1
+    num_epochs = 100
+    learning_rate = 1e-3
+    device = 'cpu'
     # model_path = os.path.abspath(os.path.join(data_dir, 'models/model.pth'))
-    net = model.GCNModel(8, 1)
+    net = model.GCNModel(dim_latent, num_layers, train=True)
     ds = dataset.PathwayDataset('demo/data/example')
+    ds_train, ds_valid = [ds[0], ds[1], ds[2]], [ds[3]]
+    dl_train = GraphDataLoader(ds_train, batch_size=1, shuffle=True)
+    dl_valid = GraphDataLoader(ds_valid, batch_size=1, shuffle=False)
 
-    pass
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
 
+    loss_per_epoch_train, loss_per_epoch_valid = [], []
+
+    for epoch in range(num_epochs):
+        loss_per_graph = []   
+        net.train() 
+        for data in dl_train:
+            graph, name = data
+            name = name[0]
+            logits = net(graph)
+            labels = graph.ndata['significance'].unsqueeze(-1)
+            loss = F.binary_cross_entropy_with_logits(logits, labels)
+
+            # Update parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Plot loss
+            loss_per_graph.append(loss.item())
+
+        loss_per_epoch_train.append(np.array(loss_per_graph).mean())
+        
+        loss_per_graph = []
+        net.eval()
+        for data in dl_valid:
+            graph, name = data
+            name = name[0]
+            logits = net(graph)
+            labels = graph.ndata['significance'].unsqueeze(-1)
+            loss = F.binary_cross_entropy_with_logits(logits, labels)
+            loss_per_graph.append(loss.item())
+
+        loss_per_epoch_valid.append(np.array(loss_per_graph).mean())
+
+    draw_loss_plot(loss_per_epoch_train, loss_per_epoch_valid, 'plot')
+
+if __name__ == '__main__':
+    train()
